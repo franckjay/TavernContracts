1bdbf64adb0335a8da991173a6c71e5ddff6017f
Timothy Roy <timothy.roy@protonmail.ch>
2022-02-25 17:40:31 -0600

    refactor: add central location to update base characters

---
+++ scripts/characters.js
@@ -0,0 +1,13 @@
+const baseCharacters = [
+  [
+    "cleric",
+    "https://avatars.githubusercontent.com/u/100328627?s=400&u=0bf6534f1115840843d47ae0192fdf8a88062f44&v=4",
+  ],
+  // ["somebodyElse", "https://avatars.githubusercontent.com/u/100328627?s=400&u=0bf6534f1115840843d47ae0192fdf8a88062f44&v=4"]
+];
+
+const roleToIndex = { cleric: 0 };
+
+// old syntax lol
+exports.baseCharacters = baseCharacters;
+exports.roleToIndex = roleToIndex;
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,36 @@
+const { baseCharacters, roleToIndex } = require("./characters");
+
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory(
     "RaiderOnboarding"
   );
+  const roles = baseCharacters.map((char) => char[0]);
+  const avatars = baseCharacters.map((char) => char[1]);
   const gameContract = await gameContractFactory.deploy(
-    [
-      "https://avatars.githubusercontent.com/u/100328627?s=400&u=0bf6534f1115840843d47ae0192fdf8a88062f44&v=4", // avatars
-    ],
-    ["cleric"], // roles
+    roles,
+    avatars,
     "GMT-6" // timezone
   );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 
+  // imagine the bot sent us this:
+  const choices = {
+    role: "cleric",
+    name: "Dan",
+    skills: "dancing, singing",
+    zone: "GMT-4",
+  };
+
+  const { role, name, skills, zone } = choices;
+
   // minting the character
-  let txn = await gameContract.mintCharacterNFT(0, "Dan", ["dancing"], "GMT-4");
+  let txn = await gameContract.mintCharacterNFT(
+    roleToIndex[role],
+    name,
+    skills,
+    zone
+  );
   await txn.wait();
 
   // Get the value of the NFT's URI.
